8d9f96f2ec3e3d56806e42a7ffcc5c03f8549634
Ryan Irujo <ryan.irujo@gmail.com>
2015-09-16 16:47:40 +0200

    Adding image and index.php to repo

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<style>
+  section {
+    position: absolute;
+    vertical-align: top;
+    top: 10%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
+  <head>
+    <title>Apache + PHP running on Docker in Azure</title>
+  <body>
+<section>
+  <img src="https://raw.githubusercontent.com/starkfell/azure-quickstart-templates/master/deploy_apache_on_docker/deploy_apache_on_docker/Scripts/ARM_template_formula.jpg">
+</section>
+
+<?php /* Yes the formatting of the image and the use of '<br>' tags below is ghetto, but honey badger don't care as this is for demo purposes only. */ ?>
+
+<br><br><br><br><br><br><br><br><br><br<br><br><br><br><br<br><br><br><br><br<br><br>
+
+<?php phpinfo(); ?>
+
+</body>
+</html>
